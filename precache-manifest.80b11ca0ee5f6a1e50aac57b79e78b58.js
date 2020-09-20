6a07457d98e4ef9b4922d27ee358aeaac73dac62
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-09-20 23:41:41 +0200

    Updates

---
+++ precache-manifest.80b11ca0ee5f6a1e50aac57b79e78b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a08f6eac0f95d4431cc947f86837812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4138dc6f9d4b6f13ceb",
+    "url": "/static/css/2.f278118f.chunk.css"
+  },
+  {
+    "revision": "43760829c3c25f3a0ac8",
+    "url": "/static/css/main.09b1a919.chunk.css"
+  },
+  {
+    "revision": "c4138dc6f9d4b6f13ceb",
+    "url": "/static/js/2.f38b8afa.chunk.js"
+  },
+  {
+    "revision": "4e4e0cc1781d9df56b5dd3d1dd940f36",
+    "url": "/static/js/2.f38b8afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43760829c3c25f3a0ac8",
+    "url": "/static/js/main.c06f2c25.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
